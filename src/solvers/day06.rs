771682b036a8e11c76dabc6c98d7be966cef59bd
Turtvaiz <4550061+vaisest@users.noreply.github.com>
2024-12-07 01:55:12 +0200

    day 6

--- src/solvers/day06.rs
+++ src/solvers/day06.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeSet;
 
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
 enum Direction {
     UP,
     DOWN,
@@ -34,16 +35,16 @@ impl Coord {
     }
     fn apply_dir(&mut self, dir: &Direction) {
         match dir {
-            Direction::UP => self.y -= 1,
+            Direction::UP => self.y = self.y.wrapping_sub(1),
             Direction::RIGHT => self.x += 1,
             Direction::DOWN => self.y += 1,
-            Direction::LEFT => self.x -= 1,
+            Direction::LEFT => self.x = self.x.wrapping_sub(1),
         }
     }
-    fn next_pos_by_dir(&self, dir: &Direction) -> Self {
+    fn next_pos_towards(&self, dir: &Direction) -> Self {
         match dir {
             Direction::UP => Coord {
-                y: self.y - 1,
+                y: self.y.wrapping_sub(1),
                 x: self.x,
             },
             Direction::RIGHT => Coord {
@@ -56,123 +57,152 @@ impl Coord {
             },
             Direction::LEFT => Coord {
                 y: self.y,
-                x: self.x - 1,
+                x: self.x.wrapping_sub(1),
             },
         }
     }
-    fn access<T: Copy>(&self, arr: &Vec<Vec<T>>) -> T {
-        arr[self.y][self.x]
+    fn get_from<'a, T: Copy>(&self, matrix: &'a Vec<Vec<T>>) -> Option<&'a T> {
+        matrix.get(self.y).and_then(|it| it.get(self.x))
     }
-    fn access_next_by_dir<'a, T: Copy>(
-        &self,
-        arr: &'a Vec<Vec<T>>,
-        dir: &Direction,
-    ) -> Option<&'a T> {
-        let pos = self.next_pos_by_dir(dir);
-        arr.get(pos.y).and_then(|it| it.get(pos.x))
+    // fn access_next_by_dir<'a, T: Copy>(
+    //     &self,
+    //     arr: &'a Vec<Vec<T>>,
+    //     dir: &Direction,
+    // ) -> Option<&'a T> {
+    //     let pos = self.next_pos_towards(dir);
+    //     arr.get(pos.y).and_then(|it| it.get(pos.x))
+    // }
+    // fn access_next_after_turn<'a, T: Copy>(
+    //     &self,
+    //     arr: &'a Vec<Vec<T>>,
+    //     dir: &Direction,
+    // ) -> Option<&'a T> {
+    //     let new_dir = next_dir(dir);
+    //     let pos = self.next_pos_by_dir(&new_dir);
+    //     arr.get(pos.y).and_then(|it| it.get(pos.x))
+    // }
+}
+
+fn walk(mut pos: Coord, matrix: &Vec<Vec<char>>) -> BTreeSet<Coord> {
+    let mut visited = BTreeSet::new();
+    let mut dir = Direction::UP;
+    while pos.in_bounds(matrix.len()) {
+        visited.insert(pos);
+        if pos
+            .next_pos_towards(&dir)
+            .get_from(&matrix)
+            .is_some_and(|&it| it == '#')
+        {
+            dir = next_dir(&dir);
+        }
+        pos.apply_dir(&dir);
     }
-    fn access_next_after_turn<'a, T: Copy>(
-        &self,
-        arr: &'a Vec<Vec<T>>,
-        dir: &Direction,
-    ) -> Option<&'a T> {
-        let new_dir = next_dir(dir);
-        let pos = self.next_pos_by_dir(&new_dir);
-        arr.get(pos.y).and_then(|it| it.get(pos.x))
-    }
+    visited
 }
 
 pub fn part1(input: String) -> String {
-    let in_arr = input
+    let matrix = input
         .lines()
         // could be arrayvec, lines are small and random access
         .map(|it| it.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
     // assert square
-    assert!(in_arr.iter().all(|it| it.len() == in_arr.len()));
+    assert!(matrix.iter().all(|it| it.len() == matrix.len()));
 
     let mut pos = Coord::new(0, 0);
-    let mut dir = Direction::UP;
-    'outer: for y in 0..in_arr.len() {
-        for x in 0..in_arr.len() {
-            if in_arr[y][x] == '^' {
+    // find the guard's starting position
+    'outer: for y in 0..matrix.len() {
+        for x in 0..matrix.len() {
+            if matrix[y][x] == '^' {
                 pos.set(y, x);
                 break 'outer;
             }
         }
     }
 
-    let mut visited = BTreeSet::new();
-    while pos.in_bounds(in_arr.len()) {
-        let elem = pos.access(&in_arr);
-        if elem == '.' || elem == '^' {
-            visited.insert(pos);
-        }
-        if pos
-            .access_next_by_dir(&in_arr, &dir)
-            .is_some_and(|&it| it == '#')
-        {
-            dir = next_dir(&dir);
-        }
-        pos.apply_dir(&dir);
-    }
+    walk(pos, &matrix).len().to_string()
+}
 
-    visited.len().to_string()
+fn test_for_cycle(spawn_pos: &Coord, matrix: &Vec<Vec<char>>) -> bool {
+    let mut pos = spawn_pos.clone();
+    let mut dir = Direction::UP;
+    let mut visited = BTreeSet::new();
+
+    while pos.in_bounds(matrix.len()) {
+        visited.insert((dir, pos));
+
+        pos.apply_dir(&dir);
+        // edge case: multiple blockers near the guard -> loop
+        loop {
+            // move either forward or to the new direction
+            // if there's a wall ahead of us, we should turn right
+            if pos
+                .next_pos_towards(&dir)
+                .get_from(&matrix)
+                .is_some_and(|&it| it == '#')
+            {
+                dir = next_dir(&dir);
+            } else {
+                break;
+            }
+        }
+        // if we're on a visited node and going the same way,
+        // we're in a loop
+        if visited.contains(&(dir, pos)) {
+            return true;
+        }
+    }
+    // we went out of bounds without being in a loop
+    // -> succsefully traversed so no cycle
+    false
 }
 
 pub fn part2(input: String) -> String {
-    let in_arr = input
+    let mut matrix = input
         .lines()
-        // could be arrayvec, lines are small and access is very random
+        // TODO: smarter container, lines are small and access is very random
         .map(|it| it.chars().collect::<Vec<char>>())
         .collect::<Vec<Vec<char>>>();
 
     // assert square
-    assert!(in_arr.iter().all(|it| it.len() == in_arr.len()));
+    assert!(matrix.iter().all(|it| it.len() == matrix.len()));
 
     let mut pos = Coord::new(0, 0);
-    let mut dir = Direction::UP;
-    'outer: for y in 0..in_arr.len() {
-        for x in 0..in_arr.len() {
-            if in_arr[y][x] == '^' {
+    // find the guard's starting position
+    'outer: for y in 0..matrix.len() {
+        for x in 0..matrix.len() {
+            if matrix[y][x] == '^' {
                 pos.set(y, x);
                 break 'outer;
             }
         }
     }
+    let original_pos = pos.clone();
 
-    let mut visited = BTreeSet::new();
-    let mut cyclic_blocks = BTreeSet::new();
-    while pos.in_bounds(in_arr.len()) {
-        let elem = pos.access(&in_arr);
-        if elem == '.' || elem == '^' {
-            visited.insert(pos);
+    // start with getting p1 answer as it's only useful
+    // to place blockers on the path that the guard goes through
+    let visited = walk(pos, &matrix);
+
+    // println!()
+
+    let mut count = 0u32;
+    for coord in visited.iter() {
+        if *coord == original_pos {
+            continue;
         }
-        // if position in front isn't visited
-        // but two position on rights are visited (or it is a turn)
-        // we could start a cycle by inserting a turn
-        let right_turn_dir = next_dir(&dir);
-        let pos_after_turn = pos.next_pos_by_dir(&right_turn_dir);
-        let pos_after_that = pos_after_turn.next_pos_by_dir(&right_turn_dir);
-        // let pos_even_after_that = pos_after_that.next_pos_by_dir(&right_turn_dir);
-        if visited.contains(&pos_after_turn)
-            && (visited.contains(&pos_after_that) || pos_after_that.access(&in_arr) == '#')
-        {
-            cyclic_blocks.insert(pos.next_pos_by_dir(&dir));
+        if matrix[coord.y][coord.x] == '#' || matrix[coord.y][coord.x] == '^' {
+            panic!("ASASDASDASASD")
         }
-        if pos
-            .access_next_by_dir(&in_arr, &dir)
-            .is_some_and(|&it| it == '#')
-        {
-            dir = next_dir(&dir);
+        matrix[coord.y][coord.x] = '#';
+
+        if test_for_cycle(&original_pos, &matrix) {
+            count += 1;
         }
-        pos.apply_dir(&dir);
+        matrix[coord.y][coord.x] = '.';
     }
 
-    println!("{:?}", cyclic_blocks);
-
-    cyclic_blocks.len().to_string()
+    count.to_string()
 }
 
 #[cfg(test)]
